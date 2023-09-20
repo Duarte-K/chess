cfbb00992a2db9be906d57ecfab8ce5d26639316
Kauã Duarte <kaua.duarte@gfxconsultoria.com>
2023-09-20 10:28:42 -0300

    Ajustes no tabuleiro

--- chess/Program.cs
+++ chess/Program.cs
@@ -7,20 +7,24 @@ namespace chess{
         private static void Main(string[] args){
 
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //try { 
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            //    Tela.imprimirTabuleiro(tab);
-            //}catch (TabuleiroException ex){
-            //    Console.WriteLine(ex.Message);
-            //}
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             Console.ReadLine();
 
         }
